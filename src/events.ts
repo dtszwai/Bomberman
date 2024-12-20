5e66dd49659aeaf79a638eb3e9b14eb2780802f6
dtszwai <dtszwai+github@gmail.com>
2024-12-20 00:17:31 -0800

    update event types

--- src/events.ts
+++ src/events.ts
@@ -1,33 +1,34 @@
 import type { GameSnapshot } from "./game/types";
-import type {
-  RoomState,
+import {
+  AnyRoomState,
+  GameStatus,
   LobbyState,
   OperationResult,
-  GameStatus,
-  Player,
-  PlayerControls,
+  Position,
+  RoomSettings,
+  UserControls,
+  UserState,
 } from "./server/types";
 
 export const Events = {
   LOBBY_STATE: "lobbyState",
-  PLAYER_STATE: "playerState",
+  WHOAMI: "whoami",
 
   // Room Events
   CREATE_ROOM: "createRoom",
   JOIN_ROOM: "joinRoom",
   LEAVE_ROOM: "leaveRoom",
   ROOM_STATE: "roomState",
+  TOGGLE_READY: "toggleReady",
 
   // Game Flow Events
   START_GAME: "startGame",
-  GAME_STATE: "gameState",
+  GAME_SNAPSHOT: "gameSnapshot",
   GAME_PAUSED: "gamePaused",
   GAME_RESUMED: "gameResumed",
   GAME_ENDED: "gameEnded",
   ROUND_START: "roundStart",
-
-  // Player Events
-  PLAYER_CONTROLS: "playerAction",
+  USER_CONTROLS: "userControls",
 } as const;
 
 // Type for type-safety when using events
@@ -35,20 +36,21 @@ export type EventType = (typeof Events)[keyof typeof Events];
 
 // Client-to-server requests
 export interface ClientEvents {
-  [Events.CREATE_ROOM]: { maxPlayers?: number; name?: string };
-  [Events.JOIN_ROOM]: { roomId: string; seat: number };
-  [Events.PLAYER_CONTROLS]: PlayerControls;
+  [Events.CREATE_ROOM]: Partial<RoomSettings & { name: string }>;
+  [Events.JOIN_ROOM]: Position;
+  [Events.USER_CONTROLS]: UserControls;
 }
 
 // Server-to-client responses and broadcasts
 export interface ServerEvents {
-  [Events.PLAYER_STATE]: Player;
+  [Events.WHOAMI]: UserState;
   [Events.LOBBY_STATE]: LobbyState;
-  [Events.ROOM_STATE]: RoomState;
-  [Events.GAME_STATE]: GameSnapshot & { status: GameStatus };
-  [Events.CREATE_ROOM]: OperationResult<RoomState>;
-  [Events.JOIN_ROOM]: OperationResult<RoomState>;
+  [Events.ROOM_STATE]: AnyRoomState;
+  [Events.GAME_SNAPSHOT]: GameSnapshot & { status: GameStatus };
+  [Events.CREATE_ROOM]: OperationResult<AnyRoomState>;
+  [Events.JOIN_ROOM]: OperationResult<AnyRoomState>;
   [Events.LEAVE_ROOM]: OperationResult;
   [Events.START_GAME]: OperationResult;
-  [Events.GAME_ENDED]: { winnerId: string; score: number[] };
+  [Events.GAME_ENDED]: { winner: UserState; score: number[] };
+  [Events.TOGGLE_READY]: OperationResult;
 }
